06a1ef822c25c5fd2a402b9e6ac59e1d52358141
bsakari <sakaribenjamin@gmail.com>
2023-04-08 09:26:26 +0300

    Mapped application.properties into InfoApp class

--- src/main/java/com/kingwanyama/spring_boot_demo_app/customer/CustomerConfiguration.java
+++ src/main/java/com/kingwanyama/spring_boot_demo_app/customer/CustomerConfiguration.java
@@ -1,5 +1,6 @@
 package com.kingwanyama.spring_boot_demo_app.customer;
 
+import com.kingwanyama.spring_boot_demo_app.infoapp.InfoApp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +10,15 @@ import org.springframework.context.annotation.Configuration;
 public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
+
+    @Value("${info.company.name}")
+    String companyName;
     @Bean
-    CommandLineRunner commandLineRunner(){
+    CommandLineRunner commandLineRunner(InfoApp infoApp){
         return args -> {
             System.out.println("Command line runner. Hooray!.");
+            System.out.println(companyName);
+            System.out.println(infoApp);
         };
     }
 
