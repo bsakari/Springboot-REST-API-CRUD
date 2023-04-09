35d2025cdeddc08a75a29794f06c3ab77205f44e
bsakari <sakaribenjamin@gmail.com>
2023-04-09 18:12:50 +0300

    Made a Post.java a record

--- src/main/java/com/kingwanyama/spring_boot_demo_app/jsonplaceholder/Post.java
+++ src/main/java/com/kingwanyama/spring_boot_demo_app/jsonplaceholder/Post.java
@@ -2,10 +2,9 @@ package com.kingwanyama.spring_boot_demo_app.jsonplaceholder;
 
 import lombok.Data;
 
-@Data
-public class Post {
-    private final Integer userId;
-    private final Integer id;
-    private final String title;
-    private final String body;
+public record Post(Integer userId,
+        Integer id,
+        String title,
+        String body) {
+
 }
